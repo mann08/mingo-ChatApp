105a5fa2345cae264035b4860c9f55bea1d4ebff
mann08 <mann882005@gmail.com>
2026-08-16 23:51:19 +0530

    feat: create Home component to structure main page layout

---
+++ Client/src/pages/Home.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Hero from "../components/Hero";
+import FeaturedPreview from "../components/FeaturedPreview";
+import FeatureGrid from "../components/FeatureGrid";
+import GroupShowcase from "../components/GroupShowcase";
+import CTA from "../components/CTA";
+import Footer from "../components/Footer";
+
+const Home = () => {
+  return (
+    <div
+      className="theme-transition"
+      style={{ background: "var(--bg)", color: "var(--text)", minHeight: "100vh" }}
+    >
+      <Hero />
+      <FeaturedPreview />
+      <FeatureGrid />
+      <GroupShowcase />
+      <CTA />
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
